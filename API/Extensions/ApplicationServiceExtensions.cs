49789daeb44ef0e935edb12f54b78a9c7ca00a70
Matthew Perez <matthewperez23@outlook.com>
2022-10-14 21:32:40 -0500

    End of section 11

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -14,7 +14,9 @@ namespace API.Extensions
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
         {
+            services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IPhotoService, PhotoService>();
             services.AddScoped<IUserRepository, UserRepository>();
             //Below is adding auto mapper to the 
             //application service extensions
